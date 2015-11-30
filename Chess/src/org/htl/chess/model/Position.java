6b75cb07b31f2f9cef9368830ddaf6d701474f0f
Andrea <Andrea@Andrea-Laptop>
2015-11-30 09:29:10 +0100

    class Spielfeld hinzugefuegt

---
+++ Chess/src/org/htl/chess/model/Position.java
@@ -0,0 +1,27 @@
+package org.htl.chess.model;
+
+public class Position
+{
+	private byte x;
+	private byte y;
+	
+	
+	public void setX(byte x)
+	{
+		this.x=x;
+	}
+	
+	public void setY(byte y)
+	{
+		this.y=y;
+	}
+	public byte getX()
+	{
+		return x;
+	}
+	
+	public byte getY()
+	{
+		return x;
+	}
+}
---
+++ Chess/src/org/htl/chess/model/Spielfeld.java
@@ -0,0 +1,32 @@
+package org.htl.chess.model;
+
+public class Spielfeld 
+{
+	private Feld[][] mat=new Feld[10][10];
+	private boolean werAmZug;
+	
+	public void ausgabe()
+	{
+		
+	}
+	
+	public boolean schach()
+	{
+		return false;
+	}
+	
+	public boolean schachMatt()
+	{
+		return false;
+	}
+	
+	public void spielzug(String zug)
+	{
+	
+	}
+	
+	public Position schach2koordinate(String schach)
+	{
+		return null;
+	}
+}
