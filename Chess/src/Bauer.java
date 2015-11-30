1a9f61a33805497575f21892c0def3f0761a4119
Andrea <Andrea@Andrea-Laptop>
2015-11-30 09:10:30 +0100

    Signed-off-by: Andrea <Andrea@Andrea-Laptop>

---
+++ Chess/src/Bauer.java
@@ -0,0 +1,68 @@
+package src;
+
+public class Bauer extends Figur 
+{
+	// Spalten sind Buchstaben
+	// Zeilen sind Zahlen
+	// 0/0  ist links unten auf a1
+	// somit die linkeste Spalte 0 und die unterste Zeile 0
+	public boolean spielZug(Spielfeld sp, Positions von, Position nach) 
+	{
+		return (spielZugMoeglich(sp, von, nach));
+	}
+
+	public boolean spielZugMoeglich(Spielfeld sp, Position von, Position nach) 
+	{
+		boolean ersterSpielzug;
+		boolean eigener;
+		if((sp.mat[von.getX()][von.getY()].farbeWeiss != sp.mat[nach.getX()][nach.getY()].farbeWeiss)||sp.mat[von.getX()][von.getY()]==null)
+		{
+			if((Figur)sp.[von.getX()][von.getY()] instanceof Bauer)
+			{
+				//Abfrage ob erste Spielzug oder nicht
+				if ((von.getY() + 1 == nach.getY()) || (von.getY() + 2 == nach.getY())) 
+				{
+					if (von.getY() == 1) 
+					{
+						ersterSpielzug = true;
+
+					}
+					else 
+					{
+						ersterSpielzug = false;
+					}
+				}
+				else 
+				{
+					eigener = false;
+				}
+
+				// Für den 1.Spielzug
+				if (ersterSpielzug && (von.getY() + 2 == nach.getY() || von.getY() + 1 == nach.getY())) 
+				{
+					// Für die anderen Spielzüge
+					if (ersterSpielzug && (von.getY() + 2 == nach.getY())||(von.getY()+1==nach.getY())) 
+					{
+						return true;
+					}
+					else
+					{
+						if((von.getX()+1==nach.getX()||von.getX()-1==nach.getX())&&von.getY()+1==nach.getY())
+						{
+							return true;
+						}
+					}						
+				}	
+			}
+			else
+			{
+				return false;
+			}
+			
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
---
+++ Chess/src/Laeufer.java
@@ -0,0 +1,56 @@
+package src;
+
+public class Laeufer 
+{
+	public boolean spielZug(Spielfeld sp, Positions von, Position nach)
+	{
+			return spielZugMoeglich();
+	}
+
+	public boolean spielZugMoeglich(Spielfeld sp, Position von, Position nach) 
+	{
+		int bewegenX=nach.getX()-von.getX();
+		int bewegenY=nach.getY()-von.getY();
+		//abfragen ob die Figur die eigene ist oder wo die Figur hingesetzt wird
+		if (sp.mat[von.getX()][von.getY()].farbeWeiss == sp.mat[nach.getX()][nach.getY()].farbeWeiss) 
+		{
+			return false;
+		}
+		else
+		{
+			//Abfragen ob die Figur ein Laeufer ist oder nicht
+			if((Figur)sp.[von.getX()][von.getY()] instanceof Laeufer)
+			{
+				if(bewegenX>0 && bewegenY>0)
+				{
+					//für vorwerts bewegen
+					if((von.getX()+bewegenX) ==nach.getX() && (von.getY()+bewegenY==nach.getY()))
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+				else
+				{
+					//für rückwerts bewegen
+					if((von.getX()-bewegenX) ==nach.getX() && (von.getY()-bewegenY==nach.getY()))
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+			else
+			{
+				return false;
+			}
+	
+		}
+	}
+}
