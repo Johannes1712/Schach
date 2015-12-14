3b4ce76308c492ba8aaaea69198e9984cf06e86a
albert <albert@jaco>
2015-12-14 08:55:26 +0100

    Moved spielzugMoeglich to class Figur

--- Chess/src/org/htl/chess/model/Springer.java
+++ Chess/src/org/htl/chess/model/Springer.java
@@ -2,20 +2,7 @@ package org.htl.chess.model;
 
 public class Springer extends Figur{
 	
-	private Feld[ ] [ ] spielfeld;
-	
-	public boolean spielZug(Spielfeld sp, Position von, Position nach){
-		
-		spielfeld= sp.getMat();
-		
-		if(this.spielzugMoeglich(sp,von,nach)){
-			
-			sp.spielzugAusfuehren(von, nach, this );
-			return true;
-		}
-		
-		return false;
-	}
+
 	
 	public boolean spielzugMoeglich(Spielfeld sp, Position von, Position nach){
 		
