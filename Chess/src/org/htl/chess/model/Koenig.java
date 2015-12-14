16e9369c04ff547cd0e08670330d11ff286dd223
Kurt Brennecke <Kurt Brennecke@Greedo>
2015-12-14 08:33:51 +0100

    Koenig

--- Chess/src/org/htl/chess/model/Koenig.java
+++ Chess/src/org/htl/chess/model/Koenig.java
@@ -1,59 +1,72 @@
 package org.htl.chess.model;
-import Figur;
-import Position;
-import SpielFeld;
 
 public class Koenig extends Figur
 {
+	private Feld [ ] [ ] spielfeld;
 
-	public boolean spielzug(SpielFeld sp,Position von,Position nach)
+	public boolean spielzug(Spielfeld sp,Position von,Position nach)
 	{
-		//Spielzug wird durchgeführt (return true) wenn der Spielzug möglich ist (spielzugMoeglich = true)
+		spielfeld= sp.getMat();
 		
-		return (spielzugMoeglich(sp,von,nach));
+		if(this.spielzugMoeglich(sp,von,nach)){
+			
+			sp.spielzugAusführen(von, nach, this );
+			return true;
+		}
 		
+		return false;
 	}
 	
-	public boolean spielzugMoeglich(SpielFeld sp,Position von,Position nach)
+	public boolean spielzugMoeglich(Spielfeld sp,Position von,Position nach)
 	{
 		
-		boolean rw = true;									//Rückgabewert
+		boolean rw = true;										//Rueckgabewert
 		
-															
-		if  ((von.x() > nach.x() +1)						//König darf nicht mehr als 1 Feld in x-Richtung gehen						
-				|| (von.x() < nach.x() - 1))						
+		spielfeld = sp.getMat();
+		
+		
+		
+		if  ((von.getX() > nach.getX() +1)						//Koenig darf nicht mehr als 1 Feld in X-Richtung gehen						
+				|| (von.getX() < nach.getX() - 1))						
 		{
 			rw = false;
 		}
 		
-		if ((von.y() > nach.y() + 1) 						//König kann immer nur 1 oder 0 Felder vor oder zurück gehen
-				|| (von.y() < nach.y() - 1))		
+		if ((von.getY() > nach.getY() + 1) 						//Koenig kann immer nur 1 oder 0 Felder vor oder zurück gehen
+				|| (von.getY() < nach.getY() - 1))		
 		{
 			rw = false;
 		}
 		
+		Feld figur = spielfeld [nach.getX()][nach.getY()];
+		
+		if(figur instanceof Figur)
+		{
+			boolean istGleichesTeam = ((Figur) figur).getFarbeWeiss();
+			if(istGleichesTeam)rw = false;
+		}
 		
 		//Rochaden Weiß:
 		
-															//große Rochade Weiß
-		if ((farbeWeiss() == true) && ((nach.x() == 2) && nach.y() == 0) 
-				&& (bewegt() == false) 						//König darf zuvor nicht bewegt worden sein
-				&& (sp.schach() == false)					//Rochade ist im Schach nicht erlaubt
-				&& (((sp.mat[0][0] instanceof Turm)			//Turm muss in der Ecke stehen
-				&& !(sp.mat[1][0] instanceof Figur)  		//auf den Feldern zwischen König und Turm darf keine Figur stehen
-				&& !(sp.mat[2][0] instanceof Figur) 
-				&& !(sp.mat[3][0] instanceof Figur)))) 
+																//große Rochade Weiß
+		if ((getFarbeWeiss() == true) && ((nach.getX() == 2) && nach.getY() == 0) 
+				&& (getBewegt() == false) 						//Koenig darf zuvor nicht bewegt worden sein
+				&& (sp.schach() == false)						//Rochade ist im Schach nicht erlaubt
+				&& (((spielfeld [0][0] instanceof Turm)			//Turm muss in der Ecke stehen
+				&& !(spielfeld [1][0] instanceof Figur)  		//auf den Feldern zwischen Koenig und Turm darf keine Figur stehen
+				&& !(spielfeld [2][0] instanceof Figur) 
+				&& !(spielfeld [3][0] instanceof Figur)))) 
 		{ 
 			rw = true;
 		}
 		
-															//kleine Rochade Weiß
-		if ((farbeWeiss() == true) && ((nach.x() == 6) && nach.y() == 0) 
-				&& (bewegt() == false) 
+																//kleine Rochade Weiß
+		if ((getFarbeWeiss() == true) && ((nach.getX() == 6) && nach.getY() == 0) 
+				&& (getBewegt() == false) 
 				&& (sp.schach() == false)
-				&& (((sp.mat[7][0] instanceof Turm)
-				&& !(sp.mat[5][0] instanceof Figur) 
-				&& !(sp.mat[6][0] instanceof Figur)))) 
+				&& (((spielfeld [7][0] instanceof Turm)
+				&& !(spielfeld [5][0] instanceof Figur) 
+				&& !(spielfeld [6][0] instanceof Figur)))) 
 		{ 
 			rw = true;
 		}
@@ -61,30 +74,39 @@ public class Koenig extends Figur
 		
 		//Rochaden Schwarz:
 		
-															//große Rochade Schwarz
-		if ((farbeWeiss() == false) && ((nach.x() == 1) && nach.y() == 7) 
-				&& (bewegt() == false) 
+																//große Rochade Schwarz
+		if ((getFarbeWeiss() == false) && ((nach.getX() == 1) && nach.getY() == 7) 
+				&& (getBewegt() == false) 
 				&& (sp.schach() == false)
-				&& (((sp.mat[0][7] instanceof Turm)
-				&& !(sp.mat[1][7] instanceof Figur) 
-				&& !(sp.mat[2][7] instanceof Figur) 
-				&& !(sp.mat[3][7] instanceof Figur)))) 
+				&& (((spielfeld [0][7] instanceof Turm)
+				&& !(spielfeld [1][7] instanceof Figur) 
+				&& !(spielfeld [2][7] instanceof Figur) 
+				&& !(spielfeld [3][7] instanceof Figur)))) 
 		{ 
 			rw = true;
 		}
 		
-															//kleine Rochade Schwarz
-		if ((farbeWeiss() == false) && ((nach.x() == 6) && nach.y() == 7) 
-				&& (bewegt() == false)
+																//kleine Rochade Schwarz
+		if ((getFarbeWeiss() == false) && ((nach.getX() == 6) && nach.getY() == 7) 
+				&& (getBewegt() == false)
 				&& (sp.schach() == false)
-				&& (((sp.mat[7][7] instanceof Turm)
-				&& !(sp.mat[5][7] instanceof Figur) 
-				&& !(sp.mat[6][7] instanceof Figur)))) 
+				&& (((spielfeld [7][7] instanceof Turm)
+				&& !(spielfeld [5][7] instanceof Figur) 
+				&& !(spielfeld [6][7] instanceof Figur)))) 
 		{ 
 			rw = true;
 		}
 		
-		return rw;
-			
+		
+		//Verhindern, dass sich Koenig selbst ins Schach setzt:
+		
+		sp.spielzugAusführen(von, nach, this );
+		if(sp.schach())
+		{
+			rw = false;
+			sp.spielzugAusführen(nach, von, this );
+		}
+		
+		return rw;		
 	}
 }
