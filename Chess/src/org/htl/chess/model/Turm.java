095ae7a40fee9f39b2042fbb12632d7c610d1b6a
Alexander <Alexander@PC-Alex>
2016-01-10 17:43:53 +0100

    hallo

--- Chess/src/org/htl/chess/model/Turm.java
+++ Chess/src/org/htl/chess/model/Turm.java
@@ -20,6 +20,40 @@ public class Turm extends Figur
 		}
 		else return false;
 		
+		
+		if(nach.getX()>von.getX()||nach.getY()>von.getY())
+		{
+			
+			int xkoord;
+			int ykoord;
+			
+			for(xkoord=von.getX();xkoord<=nach.getX();xkoord++)
+			{
+				for(ykoord=von.getY();ykoord<=nach.getY();ykoord++)
+				{
+					if(spielfeld[xkoord][ykoord]==spielfeld[von.getX()][von.getY()])
+					{
+						return false;
+					}
+				}
+			}
+		}
+		else
+		{
+			int xkoord;
+			int ykoord;
+			
+			for(xkoord=nach.getX();xkoord<=von.getX();xkoord++)
+			{
+				for(ykoord=nach.getY();ykoord<=von.getY();ykoord++)
+				{
+					if(spielfeld[xkoord][ykoord]==spielfeld[von.getX()][von.getY()])
+					{
+						return false;
+					}
+				}
+			}
+		}
 		if(rueckgabewert) figur=spielfeld[nach.getX()][nach.getY()];
 		
 		if(spielfeld[nach.getX()][nach.getY()] instanceof Figur)
