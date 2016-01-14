8540eac319a05c5e1e9c98935930b4a688632a8a
User <User@10.10.208.223>
2016-01-14 12:11:26 +0100

    fg

--- Chess/src/Testing/SpringerTest.java
+++ Chess/src/Testing/SpringerTest.java
@@ -3,6 +3,10 @@ package Testing;
 
 import org.htl.chess.model.Spielfeld;
 
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+
 import org.htl.chess.model.Position;
 import org.htl.chess.model.Spielfeld;
 import org.htl.chess.model.Springer;
@@ -11,7 +15,7 @@ import org.junit.Test;
 
 public class SpringerTest {
 
-	/*@Test
+	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
@@ -19,18 +23,8 @@ public class SpringerTest {
 	{
 		try
 		{
-<<<<<<< HEAD
 			Spielfeld sf = SpielFeldIO.einlesen("testdata/springer.txt");
-=======
-			Spielfeld sf = SpielFeldIO.einlesen("testdata/bauer_feld1.txt");
-<<<<<<< HEAD
-			sf.setWeissNachOben(false);
-			Bauer b = (Bauer) sf.getFeld(3, 6);
-=======
->>>>>>> branch 'master' of https://github.com/Johannes1712/Schach.git
-			//sf.setWeissNachOben(false);
 			Springer sp = (Springer) sf.getFigur(1,0);
->>>>>>> branch 'master' of https://github.com/Johannes1712/Schach.git
 
 			// rechts nach vorn
 			boolean beobachtet = sp.spielzugMoeglich(sf, new Position(1,0),
@@ -54,16 +48,15 @@ public class SpringerTest {
 	{
 		try
 		{
-			SpielFeld sf = SpielFeldIO.einlesen("testdata/bauer_feld2.txt");
-			sf.setWeissNachOben(false);
-			Bauer b = (Bauer) sf.getFeld(3, 6);
+			Spielfeld sf = SpielFeldIO.einlesen("testdata/bauer_feld2.txt");
+			Springer b = (Springer) sf.getFigur(3, 6);
 
 			// 1x Diagonal wenn gegnerische Figur auf Zielfeld steht LINKS
 			boolean beobachtet = b.spielzugMoeglich(sf, new Position(3, 6),
 					new Position(4, 5));
 			Assert.assertTrue(beobachtet);
 
-			b = (Bauer) sf.getFeld(4, 6);
+			//b = (Bauer) sf.getFigur(4, 6);
 			beobachtet = b.spielzugMoeglich(sf, new Position(4, 6),
 					new Position(4, 5));
 
@@ -73,5 +66,5 @@ public class SpringerTest {
 			e.printStackTrace();
 			Assert.fail();
 		}
-	}*/
+	}
 }
