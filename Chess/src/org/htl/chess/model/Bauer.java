f5707ddda906dad64bd9e71236659ddcd6f0871a
Andrea <Andrea@Andrea-Laptop>
2016-02-02 16:58:23 +0100

    bauer weitergearbeitet

--- Chess/src/org/htl/chess/model/Bauer.java
+++ Chess/src/org/htl/chess/model/Bauer.java
@@ -1,5 +1,7 @@
 package org.htl.chess.model;
 
+import Testing.SpielFeldIO;
+
 public class Bauer extends Figur 
 {
 	public Bauer(boolean farbe, boolean bewegt) {
@@ -10,6 +12,7 @@ public class Bauer extends Figur
 	// Zeilen sind Zahlen
 	// 0/0 ist links unten auf a1
 	// somit die linkeste Spalte 0 und die unterste Zeile 0
+	private Feld[][] spielfeld;
 
 	public boolean spielZug(Spielfeld sp, Position von, Position nach) 
 	{
@@ -20,66 +23,254 @@ public class Bauer extends Figur
 	{
 		if (!super.spielzugMoeglich(sp, von, nach))return false;
 	
-		boolean farbeSchwarz;
-		int bewegenY = nach.getX() - von.getX();
+		boolean farbeSchwarz=false;
 		int bewegenX = nach.getY() - von.getY();
+		int bewegenY = nach.getX() - von.getX();
 		
 		Figur figurVon = sp.getFigur(von.getX(), von.getY()); 
 		Feld figurNach=sp.getFeld(nach.getX(), nach.getY());
 		
-		if(figurNach instanceof Figur)
+		if(figurVon instanceof Figur && figurNach instanceof Figur)
 		{
 			if(!figurVon.dieselbeFigur((Figur)figurNach, figurVon)) return false;
 		}
 		
 		//abfragen welche Farbe die Figur hat
-		if(!figurVon.getFarbeW())farbeSchwarz= true;
-		else farbeSchwarz= false;
+		if(!((Figur) figurVon).getFarbeW())
+		{
+			farbeSchwarz	= true;
+		}
+		else
+		{
+			farbeSchwarz	= false;
+		}
 		
-		//Für Farbe weiß
+		if(farbeSchwarz)
+		{
+			if(von.getX()==1)
+			{
+				if(bewegenX==0)
+				{
+					if(bewegenY==1)
+					{
+						return true;
+					}
+					
+					if(bewegenY==2)
+					{
+						Feld figur=sp.getFeld(nach.getX()-1, nach.getY());
+						if(figur instanceof Figur)
+						{
+							return false;
+						}
+						else
+						{
+							return true;
+						}
+					}
+				}
+			}
+			else
+			{
+				if(bewegenX==0)
+				{
+					if(bewegenY==1)
+					{
+						return true;
+					}
+				}
+			}
+
+		}
+		/*
 		if(!farbeSchwarz)
+		{
+			if(von.getX()==6)
+			{
+				if(bewegenX==0)
+				{
+					if(bewegenY==-1)
+					{
+						return true;
+					}
+					
+					if(bewegenY==-2)
+					{
+						if(figurNach instanceof Figur)
+						{
+							if(!figurVon.dieselbeFigur((Figur)figurNach, figurVon))
+							{
+								return true;
+							}	
+							else
+							{
+								return false;
+							}
+						}
+					}
+				}
+			}
+			else
+			{
+				if(bewegenX==0)
+				{
+					if(bewegenY==-1)
+					{
+						return true;
+					}
+				}
+			}
+		}*/
+		
+		/*
+		//Für Farbe weiß
+		if(farbeSchwarz)
 		{ 
 			// Abfrage ob erste Spielzug der Farbe weiß
-			if(figurVon.getBewegt())
+			if(((Figur) figurVon).getBewegt())
 			{
-				if(bewegenY==-1 && bewegenX==0 && !(figurNach instanceof Figur)) return true;
-				if((bewegenX==1||bewegenX==-1) && bewegenY==-1 && figurNach instanceof Figur)return true;								
+				if((bewegenX==1||bewegenX==-1||bewegenX==0) && bewegenY==1)
+				{
+					if(figurNach instanceof Figur)
+					{
+						return true;
+					}					
+				}
+				else
+				{
+					if(bewegenX==0 && bewegenY==1)
+					{
+						if(figurNach instanceof Figur)
+						{
+							return false;
+						}
+						else
+						{
+							return true;
+						}
+					}
+					else
+					{
+						return false;						
+					}					
+				}				
 			}
-			
-			if(!figurVon.getBewegt())
+			else
 			{
-				figurVon.setBewegt(true);
-				
-				if(bewegenY==-2 && bewegenX==0 && figurNach instanceof Figur)return true;
-				if(bewegenY==-1 && bewegenX==0 && !(figurNach instanceof Figur)) return true;
-				if((bewegenX==1||bewegenX==-1) && bewegenY==-1 && figurNach instanceof Figur)return true;
-				else return false;
+				if(bewegenX==1||bewegenX==-1)
+				{
+					if(bewegenY==2||bewegenY==1)
+					{
+						if(figurNach instanceof Figur)
+						{
+							((Figur) figurVon).setBewegt(true);
+							return true;
+						}
+					
+					}
+				}
+				else
+				{
+					if(bewegenX==0)
+					{
+						if(bewegenY==2||bewegenY==1)
+						{
+
+							if(figurNach instanceof Figur)
+							{
+								return false;
+							}
+							else
+							{
+								((Figur) figurVon).setBewegt(true);
+								return true;
+							}
+						}
+					}
+					else
+					{
+						return false;
+					}
+				}
 			}
-		
-		
 		}
 		
 		//Für Farbe schwarz 
-		if(farbeSchwarz)
-		{ 
+		if(!farbeSchwarz)
+		{
 			// Abfrage ob erste Spielzug der Farbe schwarz
-			if(figurVon.getBewegt())
-			{
-				if(bewegenY==1 && bewegenX==0 && !(figurNach instanceof Figur)) return true;
-				if((bewegenX==1||bewegenX==-1) && bewegenY==1 && figurNach instanceof Figur)return true;								
+			if(((Figur) figurVon).getBewegt())
+			{			
+				if((bewegenX==1||bewegenX==-1) && bewegenY==-1)
+				{
+					if(figurNach instanceof Figur)
+					{
+						return true;
+					}
+				}
+				else
+				{
+					if(bewegenX==0 && bewegenY==-1)
+					{
+						if(figurNach instanceof Figur)
+						{
+							return false;
+						}
+						else
+						{
+							return true;
+						}
+					}
+					else
+					{
+						return false;
+					}
+				}
 			}
-			
-			if(!figurVon.getBewegt())
-			{	
-				figurVon.setBewegt(true);
-				
-				if(bewegenY==2&& bewegenX==0 && figurNach instanceof Figur)return true;
-				if(bewegenY==1 && bewegenX==0 && !(figurNach instanceof Figur)) return true;
-				if((bewegenX==1||bewegenX==-1) && bewegenY==1 && figurNach instanceof Figur)return true;
-				else return false;
+			else
+			{
+				if(bewegenX==1||bewegenX==-1)
+				{
+					if(bewegenY==-2||bewegenY==-1)
+					{
+						if(figurNach instanceof Figur)
+						{
+							((Figur) figurVon).setBewegt(true);
+							return true;	
+						}
+					}
+				}
+				else
+				{
+					if(bewegenX==0)
+					{
+						if(bewegenY==-2||bewegenY==-1)
+						{
+							if(figurNach instanceof Figur)
+							{
+								return false;
+							}
+							else
+							{
+								((Figur) figurVon).setBewegt(true);
+								return true;
+							}				
+						}
+					}
+					else
+					{
+						return false;
+					}
+				}
 			}
 		}
-		return false;
+		*/
 		
-	}	
+		/*
+		 * if(nach.getY()==7){
+		 * 
+		 * }
+		 */
+		return false;
+	}
 }
--- Chess/src/view/Frame.java
+++ Chess/src/view/Frame.java
@@ -11,8 +11,8 @@ import org.htl.chess.model.Position;
 import org.htl.chess.model.Spielfeld;
 import control.FeldListener;
 
-public class Frame{
-	
+public class Frame {
+
 	private JFrame frame;
 	private JPanel[][] panels = new JPanel[8][8];
 	private Spielfeld spielfeld;
@@ -20,114 +20,111 @@ public class Frame{
 	private JPanel jpanel;
 	private JPanel jpanelrechts;
 	private JPanel jpanellinks;
-	
-	public Frame(Spielfeld spielfeld){
-		this.spielfeld=spielfeld;
-	}
-	
 
-	public void initialize(){
-		
+	public Frame(Spielfeld spielfeld) {
+		this.spielfeld = spielfeld;
+	}
+
+	public void initialize() {
+
 		frame = new JFrame();
 		frame.setBounds(0, 0, 1350, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout());
-		
-		jpanel= new JPanel();
-		jpanel.setPreferredSize(new Dimension(100,600));
-	    jpanel.setMaximumSize(new Dimension(100,600));
-	    jpanel.setLayout(new GridLayout(8,8));
-	    
-	    
-	    jpanelrechts= new JPanel();
-		jpanelrechts.setPreferredSize(new Dimension(307,200));
-	    jpanelrechts.setMaximumSize(new Dimension(200,200));
-	    
-	    jpanellinks= new JPanel();
-		jpanellinks.setPreferredSize(new Dimension(307,200));
-	    jpanellinks.setMaximumSize(new Dimension(200,200));
-		
-		figuren= new Schachfiguren();
+
+		jpanel = new JPanel();
+		jpanel.setPreferredSize(new Dimension(100, 600));
+		jpanel.setMaximumSize(new Dimension(100, 600));
+		jpanel.setLayout(new GridLayout(8, 8));
+
+		jpanelrechts = new JPanel();
+		jpanelrechts.setPreferredSize(new Dimension(307, 200));
+		jpanelrechts.setMaximumSize(new Dimension(200, 200));
+
+		jpanellinks = new JPanel();
+		jpanellinks.setPreferredSize(new Dimension(307, 200));
+		jpanellinks.setMaximumSize(new Dimension(200, 200));
+
+		figuren = new Schachfiguren();
 		figuren.figurenLaden();
-		
+
 		spielfeldAufbauen();
-		
+
 		frame.add(jpanelrechts, BorderLayout.WEST);
 		frame.add(jpanellinks, BorderLayout.EAST);
 		frame.add(jpanel, BorderLayout.CENTER);
 		frame.setVisible(true);
-	
+
 	}
-	
-	public void spielfeldAufbauen(){
-		
+
+	public void spielfeldAufbauen() {
+
 		jpanel.removeAll();
-		
-		boolean schonAusgewaehlt=false;
+
+		boolean schonAusgewaehlt = false;
 		Position pos;
-		
-		for(int x=0;x<=7;x++)
-		{
-			for(int y=0;y<=7;y++){
-				
-				pos= new Position(x,y);
-				
-				FeldLaden feld=new FeldLaden();
-				if(y%2==0 && x%2==1 && !schonAusgewaehlt){
+
+		for (int x = 0; x <= 7; x++) {
+			for (int y = 0; y <= 7; y++) {
+
+				pos = new Position(x, y);
+
+				FeldLaden feld = new FeldLaden();
+				if (y % 2 == 0 && x % 2 == 1 && !schonAusgewaehlt) {
 					feld.feldSetzen(figuren.getList(), true, spielfeld.getFeld(x, y));
-					feld.addMouseListener(new FeldListener(pos, spielfeld, frame,this,spielfeld.getFeld(x, y)));
-					
-					schonAusgewaehlt=true;
+					feld.addMouseListener(new FeldListener(pos, spielfeld, frame, this, spielfeld.getFeld(x, y)));
+
+					schonAusgewaehlt = true;
 				}
-				if(y%2==0 && x%2==0 && !schonAusgewaehlt){
+				if (y % 2 == 0 && x % 2 == 0 && !schonAusgewaehlt) {
 					feld.feldSetzen(figuren.getList(), false, spielfeld.getFeld(x, y));
-					feld.addMouseListener(new FeldListener(pos, spielfeld, frame,this,spielfeld.getFeld(x, y)));
-					
-					schonAusgewaehlt=true;
+					feld.addMouseListener(new FeldListener(pos, spielfeld, frame, this, spielfeld.getFeld(x, y)));
+
+					schonAusgewaehlt = true;
 				}
-				if(y%2==1 && x%2==0 && !schonAusgewaehlt){
+				if (y % 2 == 1 && x % 2 == 0 && !schonAusgewaehlt) {
 					feld.feldSetzen(figuren.getList(), true, spielfeld.getFeld(x, y));
-					feld.addMouseListener(new FeldListener(pos, spielfeld, frame,this,spielfeld.getFeld(x, y)));
-					
-					schonAusgewaehlt=true;
+					feld.addMouseListener(new FeldListener(pos, spielfeld, frame, this, spielfeld.getFeld(x, y)));
+
+					schonAusgewaehlt = true;
 				}
-				if(y%2==1 && x%2==1 && !schonAusgewaehlt){
+				if (y % 2 == 1 && x % 2 == 1 && !schonAusgewaehlt) {
 					feld.feldSetzen(figuren.getList(), false, spielfeld.getFeld(x, y));
-					feld.addMouseListener(new FeldListener(pos, spielfeld, frame,this,spielfeld.getFeld(x, y)));
-					
-					schonAusgewaehlt=true;
+					feld.addMouseListener(new FeldListener(pos, spielfeld, frame, this, spielfeld.getFeld(x, y)));
+
+					schonAusgewaehlt = true;
 				}
-				
+
 				System.out.print(spielfeld.getFeld(x, y).getClass().getSimpleName());
-				
-				panels[x][y]=feld;
-				feld.setBounds(0, 0, 20,20);
-				
+
+				panels[x][y] = feld;
+				feld.setBounds(0, 0, 20, 20);
+
 				jpanel.add(panels[x][y]);
-				schonAusgewaehlt=false;
-				
+				schonAusgewaehlt = false;
+
 			}
 			System.out.println();
 		}
 		jpanel.repaint();
 		frame.setVisible(true);
 	}
-	
-	public void felderAnfaerben(ArrayList<Position> liste){
-		
-		FeldLaden feld= new FeldLaden();
-		
-		for(Position pos: liste){
-			int x= pos.getX();
-			int y= pos.getY();
-			
-			feld=(FeldLaden) panels[x][y];
+
+	public void felderAnfaerben(ArrayList<Position> liste) {
+
+		FeldLaden feld = new FeldLaden();
+
+		for (Position pos : liste) {
+			int x = pos.getX();
+			int y = pos.getY();
+
+			feld = (FeldLaden) panels[x][y];
 			feld.setFaerbig(true);
-			
+
 			System.out.println("in Methode felderAnfaerben");
-			
+
 			feld.repaint();
-			
+
 		}
 		frame.repaint();
 	}
