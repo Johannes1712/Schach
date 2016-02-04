a028869740b511ebf070bb59f93f06d8e294ba04
Kurt Brennecke <Kurt Brennecke@Greedo>
2016-02-04 11:52:00 +0100

    koenig

--- Chess/src/org/htl/chess/model/Spiel.java
+++ Chess/src/org/htl/chess/model/Spiel.java
@@ -9,7 +9,7 @@ public class Spiel {
 	public static void main(String[] args) throws FileNotFoundException
 	{
 
-		Spielfeld sf = SpielFeldIO.einlesen("Bauer");
+		Spielfeld sf = SpielFeldIO.einlesen("Spielfeld-Startposition.txt");
 		//Spielfeld sf = SpielFeldIO.einlesen("SchachRNM.txt");
 		sf.ausgabe();
 		
